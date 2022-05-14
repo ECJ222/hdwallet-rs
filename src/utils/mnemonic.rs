26dceca2e76956b77d72aa5b8d7e7b7c7e2ab4c1
Enoch Chejieh <enochchejieh@gmail.com>
2022-05-14 16:19:23 +0100

    feat: added func for mnemonics and key creation

---
+++ src/utils/mnemonic.rs
@@ -0,0 +1,31 @@
+use bip0039::{Count, Language, Mnemonic};
+use super::error::Error;
+
+/// Generate mnemonic.
+pub fn new_mnemonic(length: u32, language_of_choice: &str) -> Result<Mnemonic, Error> {
+    let count = if length <= 12 {
+        Count::Words12
+    } else {
+        Count::Words24
+    };
+
+    let language = match language_of_choice.to_lowercase().as_str() {
+        "english" => Language::English,
+        "japanese" => Language::Japanese,
+        "korean" => Language::Korean,
+        "italian" => Language::Italian,
+        "french" => Language::French,
+        "czech" => Language::Czech,
+        "chinese-simplified" => Language::SimplifiedChinese,
+        "chinese-traditional" => Language::TraditionalChinese,
+        "portuguese" => Language::Portuguese,
+        _ => return Err(Error::LanguageNotSupported),
+    };
+
+    Ok(Mnemonic::generate_in(language, count))
+}
+
+/// Generate seed phrase.
+pub fn new_seed(mnemonic: Mnemonic, password: String) -> [u8; 64] {
+     mnemonic.to_seed(password)
+}
---
+++ src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod key;
+pub mod mnemonic;
+pub mod error;
